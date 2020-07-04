a79594fb665073e7a03ee01f38222be3c0f6a6ad
Brendan M <bjohnmurphy1984@gmail.com>
2020-07-04 10:11:08 -0400

    add jest unit testing framework and got first unit test passing

--- src/GreenLightHealth.Client/wwwroot/js/site.js
+++ src/GreenLightHealth.Client/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+if (document.getElementById("accept")) {
+    document.getElementById("accept").addEventListener("click", agreeToHealthDec);
+    document.getElementById("decline").addEventListener("click", declineHealthDec);
+}
+
+//TODO figure out what needs to happen here, maybe just highlight for now
+function agreeToHealthDec() {
+    alert ("@User.Identity.Name agrees to health declaration");
+}
+    function declineHealthDec() {
+    alert ("@User.Identity.Name declines health declaration");
+}
+
+//example function for unit testing purposes
+const sum = (a, b) => a + b
+
+module.exports = { sum }
