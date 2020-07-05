48417c4b2ab95d5309287b3129c4a10bd084a18a
Brendan M <bjohnmurphy1984@gmail.com>
2020-07-05 14:18:54 -0400

    toggle red and green for stop light

--- src/GreenLightHealth.Client/wwwroot/js/site.js
+++ src/GreenLightHealth.Client/wwwroot/js/site.js
@@ -2,15 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-document.getElementById("accept").addEventListener("click", agreeToHealthDec);
-document.getElementById("decline").addEventListener("click", declineHealthDec);
-//TODO figure out what needs to happen here, maybe just highlight for now
-function agreeToHealthDec() {
-    // alert ("@User.Identity.Name agrees to health declaration");
-    $("#stopbubble").toggleClass("green",true);
+document.getElementById("accept").addEventListener("click", setElementGreen);
+document.getElementById("decline").addEventListener("click", setElementRed);
 
+function setElementRed() {
+    $("#stopbubble").removeClass("green");
+    $("#stopbubble").toggleClass("red", true);
 }
-    function declineHealthDec() {
-    // alert ("@User.Identity.Name declines health declaration");
-    $("#stopbubble").toggleClass("red",true);
-}
+function setElementGreen() {
+    $("#stopbubble").removeClass("red");
+    $("#stopbubble").toggleClass("green", true);
+ }
