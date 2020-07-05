6e119e64792b7b02d873684df2f6ea429cad9f14
Nicholas Owen Weber <noweber@gmail.com>
2020-07-04 22:02:18 -0700

    Acceptance Test to Assert First Container of Home View

--- src/GreenLightHealth.AutomatedUITests/HomeAutomatedUITests.cs
+++ src/GreenLightHealth.AutomatedUITests/HomeAutomatedUITests.cs
@@ -1,6 +1,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace GreenLightHealth.AutomatedUITests
@@ -8,10 +9,11 @@ namespace GreenLightHealth.AutomatedUITests
     public class HomeAutomatedUITests : IDisposable
     {
         private readonly IWebDriver _driver;
-        private string site = "https://localhost:5001/";
+        private string site = "https://localhost:44386/";
         public HomeAutomatedUITests()
         {
             _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl(site);
         }
 
         public void Dispose()
@@ -23,11 +25,37 @@ namespace GreenLightHealth.AutomatedUITests
         [Fact]
         public void HealthDeclaration()
         {
-            site = "https://localhost:5001/";
-            _driver.Navigate().GoToUrl(site);
-
             Assert.Equal("Green Light Healthy - Health Declaration", _driver.Title);
             Assert.Contains("Green Light? Healthy!", _driver.PageSource);
         }
+
+        [Fact]
+        public void HomeViewFirstContainerContentIsVisibleWithStoplight()
+        {
+            // Act:
+            IWebElement containerElement = _driver.FindElement(By.Id("container1"));
+            IReadOnlyCollection<IWebElement> childElements = containerElement.FindElements(By.XPath(".//*"));
+
+            // Assert:
+            Assert.NotNull(containerElement);
+            Assert.True(containerElement.Displayed);
+            Assert.True(containerElement.Enabled);
+            Assert.NotNull(childElements);
+            bool stoplightFound = false;
+            foreach(IWebElement element in childElements)
+            {
+                if(element.TagName.Contains("span"))
+                {
+                    Assert.True(element.Displayed);
+                    Assert.True(element.Enabled);
+                    string classes = element.GetAttribute("class");
+                    Assert.Contains("stoplight", classes);
+                    stoplightFound = true;
+                }
+            }
+            Assert.True(stoplightFound);
+        }
+
+
     }
 }
