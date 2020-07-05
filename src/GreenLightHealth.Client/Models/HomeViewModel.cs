0cdbcc0228e8fa116a27fd767888a3a7c05002a7
Nicholas Owen Weber <noweber@gmail.com>
2020-07-05 12:40:22 -0700

    Revisions to the Home View and Model for English-Only Text w/i Health Declaration and Buttons

--- src/GreenLightHealth.Client/Models/HomeViewModel.cs
+++ src/GreenLightHealth.Client/Models/HomeViewModel.cs
@@ -9,6 +9,12 @@ namespace GreenLightHealth.Client.Models
 
         public string DeclineId = "decline";
 
-        public string HealthDeclaration = "For the health and safety of our community, declaration of illness is required. Be sure that the information you'll give is accurate and complete. Please get immediate medical attention if you have any of the COVID-19 signs.";
+        public string HealthDeclarationHeader = "Are You Healthy?";
+
+        public string HealthDeclarationParagraph = "For the health and safety of our community, declaration of illness is required. Be sure that the information you'll give is accurate and complete. Please get immediate medical attention if you have any of the COVID-19 signs.";
+
+        public string AcceptText = "Accept";
+
+        public string DeclineText = "Decline";
     }
 }
